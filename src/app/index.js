138bc706a1f8a4dd9ebf14ab523edc76458febe8
MitchRivet <mitch.rivet@proseeder.com>
2018-01-23 21:05:47 -0800

    add babel and start scripts

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,10 @@
-console.log('webpack is working')
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return <p> Nike Test</p>;
+  }
+}
+
+render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
 
-var BUILD_DIR = path.resolve(__dirname, 'src/public');
-var APP_DIR = path.resolve(__dirname, 'src/app');
+var BUILD_DIR = path.resolve(__dirname, "src/public");
+var APP_DIR = path.resolve(__dirname, "src/app");
 
 var config = {
-  entry: APP_DIR + '/index.js',
+  entry: APP_DIR + "/index.js",
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015"]
+        }
+      }
+    ]
   }
 };
 
